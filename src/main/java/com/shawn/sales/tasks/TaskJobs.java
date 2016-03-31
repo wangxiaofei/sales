9845a4d5a095a1a9ad91537bc8f5df4e09b0a9c6
wangxiaofei <wangxiaofei340@163.com>
2016-03-31 23:10:21 +0800

    增加线程试验

--- src/main/java/com/shawn/sales/tasks/TaskJobs.java
+++ src/main/java/com/shawn/sales/tasks/TaskJobs.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
 import com.shawn.sales.business.IMailer;
@@ -32,15 +33,36 @@ public class TaskJobs {
 	private UserDao userDao;
 	@Autowired
 	private CustomerDao cusDao;
+	@Autowired
+	private ThreadPoolTaskExecutor taskExecutor;
 
-	// @Scheduled(cron = "0/5 * * * * *")
+	 @Scheduled(cron = "0/5 * * * * *")
 	void doSomethingWith() {
 		// System.out.println("I'm doing with cron now!");
 		// logger.debug("log-I'm doing with cron now!");
-		// monitor.info("test");
+		 monitor.info("test");
+		 
+		 taskExecutor.execute(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				monitor.info("task....");
+				monitor.info("getActiveCount="+taskExecutor.getActiveCount());
+				monitor.info("getMaxPoolSize="+taskExecutor.getMaxPoolSize());
+				monitor.info("getPoolSize="+taskExecutor.getPoolSize());
+				monitor.info("getCorePoolSize="+taskExecutor.getCorePoolSize());
+				try {
+					Thread.sleep(100000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
-	@Scheduled(cron = "0 0 12 * * ?")
+//	@Scheduled(cron = "0 0 12 * * ?")
 	void checkUpdateProduct() {
 		logger.info("检查更新产品...");
 		List<SaleRecord> saleList = saleDao.getNeedUpdateList();
